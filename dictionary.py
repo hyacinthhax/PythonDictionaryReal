b49282cac412e39318d39273f621f1f292c1ab2c
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2023-02-05 20:35:30 -0700

    Update dictionary.py
    Made Shorter and actually works. Can ask it as a basic repo of human language, now to make something understand it.

--- dictionary.py
+++ dictionary.py
@@ -1,66 +1,50 @@
 import re
-import time
-from PyDictionary import PyDictionary
 
-# Dictionary Setup
 
-dictionary = {}
+wordsList = []
+defList = []
 originalDictionary = []
-wordList = []
+definition = ""
+newDict = {}
 
+with open('Dictionary/dictionary.txt', 'r') as f:
+	for lines in f.readlines():
+		originalDictionary.append(lines)
+# print(list(originalDictionary))
+    
 regex_patterns = {
-    'word': r'(\w+)\n',
-    'deff': r'(\d.*|Defn.*)\n',
-    'defcont': r'(\w+\w*)\n'
-    }
+	'word': r"(\w+)\n",
+}
+
+for lines in originalDictionary:
+	if lines != "\n":
+		for key, value in regex_patterns.items():
+			testset = 0
+			intent = key
+			regex_pattern = value
+			found_match = re.match(regex_pattern, lines)
+			if found_match:
+				if intent == 'word':
+					definition = ""
+					count = +1
+					word = found_match.groups()[0]
+					wordsList.append(word)
+					testset = 0
+
+			else:
+				testset = 1
+				definition = definition + " " + lines.strip('\n')
+	newDict[word] = definition
+	# defList.append(definition)
 
 
-def run():
-    deffList = []
-
-    def innerFunction(word):
-        if word != "":
-            print(word)
-            dictionary[word] = " ".join(deffList)
-
-    with open('dictionary.txt', 'r') as f:
-        for lines in f.readlines():
-            originalDictionary.append(lines)
-        
-    with open('words.txt', 'r') as fr:
-        count = 0
-        for line in originalDictionary:
-            for key, value in regex_patterns.items():
-                intent = key
-                regex_pattern = value
-                found_match = re.match(regex_pattern, line)
-                if found_match:
-                    if intent == 'word':
-                        word = found_match.groups()[0]
-                        count +=1
-                        innerFunction(word)
-                        deffList = []
-                        # time.sleep(0.01)
-                        
-                        
-                    else:
-                        data = line.strip('\n')
-                        if data not in deffList:
-                            deffList.append(data)
-
-                
-                else:
-                    data = line.strip('\n')
-                    if data not in deffList:
-                        deffList.append(data)
-
-
-run()
-print(list(dictionary))
-
-
-with open('DictionaryReal.py', 'a') as fw:
-    fw.write('pydictionary = {\n')
-    for key, value in dictionary.items():
-        fw.write(f'{key}: {value},\n')
-    fw.write('}')
+# while True:
+# 	print(newDict[input("Word:  ").upper()])
+# print(len(list(wordsList)))
+# print(list(defList))
+# 
+with open('Dictionary.py', 'w') as fw:
+	fw.write('RealDict = {\n')
+	for words, definitions in newDict.items():
+		fw.write(f"{words}:{definitions},\n")
+	fw.write('}')
