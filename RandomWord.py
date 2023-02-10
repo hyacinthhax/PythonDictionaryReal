9ab13cadaaee448cb34400270a38246d7deacee9
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2023-02-10 14:12:33 -0700

    Update RandomWord.py
    Instant section complete, on to Scheduled Section

--- RandomWord.py
+++ RandomWord.py
@@ -13,9 +13,17 @@ def run():
     # This secttion is for instant run
     while True:
         randnumber = random.randint(0, 103136)
-        word = wordList[randnumber]
-        input("Press Enter To Continue...  ")
-        print(f"{word}:{list(realDict[word])}")
+        try:
+            randomword = wordList[randnumber]
+            word = input("Press Enter To Continue...  \nOr Ask a Follow up word...  >")
+            if word == "":
+                print(f"{randomword}:{list(realDict[randomword])}")
+            elif word != "":
+                print(f"{word.upper()}: {list(realDict[word.upper()])}")
+
+        except KeyError:
+            print(f"{word} Not Found in realDict...  ")
+            return run()
 
 
 run()
