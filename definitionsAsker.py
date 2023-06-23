f9d625f08a06e305d952bb4bbceb9b7d38ad2763
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2023-06-23 10:17:01 -0700

    Add files via upload

---
+++ definitionsAsker.py
@@ -0,0 +1,27 @@
+from results import *
+import random
+
+
+# We import a wordList to make selections and randoms
+wordList = []
+with open('words.txt', 'r') as f:
+    for data in f.read().split('\n'):
+        data = data.strip('\n')
+        wordList.append(data)
+
+# We Loop through and select a random word if empty string
+while True:
+    print("Welcome to the Free Dictionay. Type your word and get a definition...")
+    word = input("-->")
+    try:
+        if word == "":
+            randnumber = random.randint(0, 103136)
+            randomword = wordList[randnumber]
+            print(f"{randomword}:{list(realDict[randomword])}")
+        elif word != "":
+            print(f"{word.upper()}: {list(realDict[word.upper()])}")
+
+        input("Enter to Continue...  ")
+
+    except KeyError:
+        print(f"{word} Not Found in realDict...  ")
