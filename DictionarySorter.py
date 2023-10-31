b9c9b988391239b6e2a74ece97d6fc5ff4ef3dc7
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2023-10-30 21:10:38 -0700

    Update DictionarySorter.py
    Added changes to include full text of definition, next to include refined definition and split if # from 1-X

--- DictionarySorter.py
+++ DictionarySorter.py
@@ -1,58 +1,80 @@
 import re
-from textcleaner import cleaner
 
-
-wordsList = []
-defList = []
-originalDictionary = []
-definition = ""
-word = ""
-newDict = {}
-
-with open('dictionary.txt', 'r') as f:
-	for lines in f.readlines():
-		originalDictionary.append(lines)
-# print(list(originalDictionary))
+# Dictionary Setup
+dictionary = {}
 
 regex_patterns = {
-	'word': r'([A-Z-]+)\n',
-	'definition': r"(.+ .+ .+ .*)\n"
+    'word': r'([A-Z-]+)\n',
+    'restricted': r'-+'
 }
 
-for lines in originalDictionary:
-	if lines != "\n":
-		for key, value in regex_patterns.items():
-			testset = 0
-			intent = key
-			regex_pattern = value
-			found_match = re.match(regex_pattern, lines)
+def parseDefinition(definition_line):
+    definition = definition_line.strip()  # Remove leading and trailing whitespace
+    definition = re.sub(r'[^\w\s.,-]', '', definition)  # Remove unwanted characters
+    return definition
 
-			if found_match:
-				if intent == 'word':
-					count = +1
-					word = found_match.groups()[0]
-					if word != '':
-						if word not in wordsList:
-							wordsList.append(word)
-							definition = ""
-							testset = 0
-						elif word in wordsList:
-							testset = 1
+def joinAndCleanDefinitions(definitions):
+    joined_definitions = ' '.join(definitions)  # Join definitions into a single string
+    split_definitions = [d.strip() for d in re.split(r'\d+\.', joined_definitions) if d.strip()]  # Split by numbers and remove empty lines
+    return split_definitions
 
-				if intent == 'definition':
-					testset = 1
-					definition = definition + found_match.groups()[0] + " "
+import re
+
+def parseDictionary():
+    with open('Dictionary/dictionary.txt', 'r') as f:
+        lines = f.readlines()
+
+    current_word = None
+    prev_word = None
+    current_definitions = []
+    begin_count = 0
+
+    for line in lines:
+        for words in line:
+                words.replace('-', '')
+        word_match = re.search(regex_patterns['word'], line)
+        if word_match:
+            word = word_match.group(1).strip()
+            if not re.search(r'[-]{2,100}', word):
+                print(begin_count)
+                # Exclude words with hyphens, --, or ---
+                if prev_word is not None or begin_count > 0:
+                    prev_word = current_word
+                current_word = word
+                begin_count += 1
+                if current_word not in dictionary:
+                    dictionary[current_word] = []
+                    current_definitions = []
+
+                elif current_word in dictionary:
+                    current_definitions = []
+
+        elif not word_match:
+            definition = parseDefinition(line)
+            current_definitions.append(definition)
+            my_set = set(dictionary[current_word])
+            my_set.update(current_definitions)
+            dictionary[current_word] = list(my_set)
+
+    for words in dictionary.keys():
+        print(words)
+        joined_and_cleaned_definitions = joinAndCleanDefinitions(dictionary[words])
+        if joined_and_cleaned_definitions:
+            dictionary[words] = joined_and_cleaned_definitions
 
 
-		newDict[word] = definition
+def saveDictionaryToFile():
+    with open("Dictionary.py", 'w') as wb:
+        wb.write("dictionary = {\n")
+        for words, defs in dictionary.items():
+            wb.write(f"'{words}': {defs}, \n")
 
+        wb.write("}")
 
-with open('Dictionary.py', 'w') as fw:
-	fw.write("realDict = {\n")
-	for words, definitions in newDict.items():
-		fw.write(f"'{words}':{cleaner(definitions)},\n")
-	fw.write('}')
+def main():
+    parseDictionary()
+    saveDictionaryToFile()
 
-with open('words.txt', 'w') as fww:
-	for keys in newDict.keys():
-		fww.write(f"{keys}\n")
+if __name__ == "__main__":
+    main()
+
