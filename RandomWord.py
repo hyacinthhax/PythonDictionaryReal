6880b70f4de12a8e62c43d7841bdde79d379f561
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2024-12-17 18:46:46 -0700

    Fixed RandomWord.py
    Also the main search function

--- RandomWord.py
+++ RandomWord.py
@@ -1,25 +1,22 @@
-from results import *
+from Dictionary import realDict
 import random
 
 
-wordList = []
-with open('words.txt', 'r') as f:
-    for data in f.read().split('\n'):
-        data = data.strip('\n')
-        wordList.append(data)
 
 def run():
+    wordList = list(realDict.keys())
+    max_num = len(list(realDict.keys()))
     # print(len(list(wordList)))
     # This secttion is for instant run
     while True:
-        randnumber = random.randint(0, 94251)
+        randnumber = random.randint(0, max_num)
         try:
             randomword = wordList[randnumber]
             word = input("Press Enter To Continue...  \nOr Ask a Follow up word...  >")
             if word == "":
-                print(f"{randomword}:{list(realDict[randomword])}")
+                print(f"{randomword}:{realDict[randomword]}")
             elif word != "":
-                print(f"{word.upper()}: {list(realDict[word.upper()])}")
+                print(f"{word.upper()}: {realDict[word]}")
 
         except KeyError:
             print(f"{word} Not Found in realDict...  ")
